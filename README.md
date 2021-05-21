58072f6cda44a10ad05f8a9c8c2fa4f9be6245bc
astrogewgaw <ujjwalpanda97@gmail.com>
2021-05-21 17:08:19 +0530

    :recycle: Refactoring how scraping works.

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 [![Code style: black][black-badge]][black]
 
-this is my own copy of the RRATs database maintained [**here**][rrats] by Bingyi Cui and Maura McLaughlin. the data in the original catalogue might be out-of-date now, since it was updated last in September 2016. nevertheless, this repository scraps the database on the 12th of every month, at midnight, and stores all the data in full JSONic glory in [**rrats.json**](rrats.json). the code that runs this repository is in [**scrap.py**](scrap.py). this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
+this is my own copy of the RRATs database maintained [**here**][rrats] by Bingyi Cui and Maura McLaughlin. the data in the original catalogue might be out-of-date now, since it was updated last in September 2016. nevertheless, this repository scraps the database on every Friday, at midnight (in UTC time), and stores all the data in full JSONic glory in [**rrats.json**](rrats.json). the code that runs this repository is in [**scrap.py**](scrap.py). this repository will eventually power the [**koshka**][koshka] package, which aims to make accessing all pulsar and radio transient related catalogues easier.
 
 [black]: https://github.com/psf/black
 [rrats]: http://astro.phys.wvu.edu/rratalog/
--- scrap.py
+++ scrap.py
@@ -1,72 +1,64 @@
-import re
-import json
-import requests
-
-from typing import Optional
-from schema import Use, Schema
-
-# URL of the RRATs catalog.
-url = "http://astro.phys.wvu.edu/rratalog/rratalog.txt"
-
-
-def validator(s: str) -> Optional[float]:
-
-    """
-    A validator to deal with numeric values that could possibly be NULL.
-    """
-
-    if s == "--":
-        return None
-    else:
-        return float(s)
-
-
-# The key-value map for the database.
-kvs = {
-    "NAME": Use(str),
-    "P": Use(validator),
-    "PDOT": Use(validator),
-    "DM": Use(validator),
-    "RA": Use(str),
-    "DEC": Use(str),
-    "GAL_L": Use(validator),
-    "GAL_B": Use(validator),
-    "RATE": Use(validator),
-    "LOG_B": Use(validator),
-    "LOG_TS": Use(validator),
-    "D_HAT": Use(validator),
-    "FLUX_D": Use(validator),
-    "W": Use(validator),
-    "SURVEY": Use(str),
-}
-
-
-def scrap() -> None:
-
-    """
-    Scrap the RRATs database and serialise it into a JSON file.
-    """
-
-    cat = requests.get(url)
-    data = cat.content.decode()
-    rows = [
-        [_ for _ in re.split(r"\s+|\t+|\r+", _) if _]
-        for _ in re.split(r"\n+", data)[1:-1]
-    ]
-    rratp = lambda keys, vals: {key: val for key, val in zip(keys, vals)}
-    rrats = {
-        str(i + 1): Schema(kvs).validate(rratp(kvs.keys(), row))
-        for i, row in enumerate(rows)
-    }
-
-    with open("rrats.json", "w+") as fobj:
-        json.dump(
-            rrats,
-            fobj,
-            indent=4,
-        )
-
-
 if __name__ == "__main__":
 
-    scrap()
+    from json import dump
+    from requests import get
+    from bs4 import BeautifulSoup
+    from typing import Dict, Union, Optional
+
+    data: Dict[
+        str,
+        Dict[
+            str,
+            Optional[
+                Union[
+                    str,
+                    int,
+                    float,
+                ]
+            ],
+        ],
+    ] = {}
+
+    rows = BeautifulSoup(
+        markup=get("http://astro.phys.wvu.edu/rratalog/").content,
+        features="lxml",
+    ).table("tr")[2:]
+
+    for i, row in enumerate(rows):
+        cells = [_.text for _ in row("td")]
+        data[str(i + 1)] = {
+            key: (None if (cell == "--") or (cell == "") else conv(cell))
+            for (key, conv), cell in zip(
+                [
+                    ("NAME", str),
+                    ("P0", float),
+                    ("P1", float),
+                    ("DM", float),
+                    ("RAJ", str),
+                    ("DECJ", str),
+                    ("GL", float),
+                    ("GB", float),
+                    ("BURST_RATE", float),
+                    ("LOG_B", float),
+                    ("LOG_AGE", float),
+                    ("DIST", float),
+                    ("S140", float),
+                    ("S350", float),
+                    ("S1400", float),
+                    ("W140", float),
+                    ("W350", float),
+                    ("W1400", float),
+                    ("RM", float),
+                    ("LIN_POL", float),
+                    ("SURVEY", str),
+                ],
+                cells,
+            )
+        }
+
+    with open("rrats.json", "w+") as fobj:
+        dump(
+            obj=dict(data=data),
+            fp=fobj,
+            indent=4,
+        )
